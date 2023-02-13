64237070a3802f2ed7c1ded6d31faf1db32bc2fc
LowerDeckBoy <pa.krawiec@onet.pl>
2023-02-13 12:58:53 +0100

    Minor updates

--- DirectX 12 Rendering/Assets/Shaders/PS_Texture.hlsl
+++ DirectX 12 Rendering/Assets/Shaders/PS_Texture.hlsl
@@ -10,5 +10,5 @@ SamplerState samplerState : register(s0);
 
 float4 PS(PS_Input pin) : SV_TARGET
 {
-    return tex.Sample(samplerState, pin.UV);
+    return float4(tex.Sample(samplerState, pin.UV).rgb, 1.0f);
 }
