9cac3e7b9576d2e2fde85cb6bb59a89614d7ca5e
LowerDeckBoy <pa.krawiec@onet.pl>
2023-04-10 19:13:54 +0200

    Little overhaul

--- DirectX 12 Rendering/Source/Graphics/Shader.hpp
+++ DirectX 12 Rendering/Source/Graphics/Shader.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <d3d12.h>
-//#include <d3dcompiler.h>
+#include <d3dcompiler.h>
 #include <wrl.h>
 
 class VertexShader
@@ -20,13 +20,14 @@ public:
 
 		if (FAILED(hResult) || Blob == nullptr)
 		{
-			OutputDebugStringA("Failed to compile Vertex Shader!\n");
+			::OutputDebugStringA(static_cast<char*>(Blob->GetBufferPointer()));
+			::OutputDebugStringA("Failed to compile Vertex Shader!\n");
 			return;
 		}
 
 		if (Error != nullptr)
 		{
-			OutputDebugStringA(static_cast<char*>(Error->GetBufferPointer()));
+			::OutputDebugStringA(static_cast<char*>(Error->GetBufferPointer()));
 			return;
 		}
 
@@ -66,13 +67,13 @@ public:
 
 		if (FAILED(hResult) || Blob == nullptr)
 		{
-			OutputDebugStringA("Failed to compile Vertex Shader!\n");
+			::OutputDebugStringA("Failed to compile Pixel Shader!\n");
 			return;
 		}
 
 		if (Error != nullptr)
 		{
-			OutputDebugStringA(static_cast<char*>(Error->GetBufferPointer()));
+			::OutputDebugStringA(static_cast<char*>(Error->GetBufferPointer()));
 			return;
 		}
 
@@ -85,5 +86,45 @@ public:
 private:
 	Microsoft::WRL::ComPtr<ID3DBlob> Blob;
 	Microsoft::WRL::ComPtr<ID3DBlob> Error;
+	bool bIsInitialized{ false };
+};
+
+class Shader
+{
+public:
+	void Create(const std::string_view& Filepath, const std::string_view& Entrypoint, const std::string_view& Target)
+	{
+		if (bIsInitialized)
+			return;
+
+		uint32_t compileFlags{ 0 };
+#if defined (_DEBUG) || DEBUG
+		compileFlags |= D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+		ID3DBlob* Error{ nullptr };
+		std::wstring path{ std::wstring(Filepath.begin(), Filepath.end()) };
+		HRESULT hResult{ D3DCompileFromFile(path.c_str(), nullptr, nullptr, Entrypoint.data(), Target.data(), compileFlags, 0, Blob.GetAddressOf(), &Error)};
+
+		if (Error != nullptr)
+		{
+			::OutputDebugStringA(static_cast<char*>(Error->GetBufferPointer()));
+			return;
+		}
+
+		if (FAILED(hResult) || Blob == nullptr)
+		{
+			::OutputDebugStringA("Failed to compile shader!\n");
+			return;
+		}
+
+		bIsInitialized = true;
+	}
+
+	[[nodiscard]]
+	inline ID3DBlob* GetData() const { return Blob.Get(); }
+
+
+private:
+	Microsoft::WRL::ComPtr<ID3DBlob> Blob;
 	bool bIsInitialized{ false };
 };
