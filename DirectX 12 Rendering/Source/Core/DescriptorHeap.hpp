8db70eecd1aa6c22d697ea24215d9b380f1c3a17
LowerDeckBoy <pa.krawiec@onet.pl>
2023-04-10 19:11:55 +0200

    Added getters and setter + little cleanup

--- DirectX 12 Rendering/Source/Core/DescriptorHeap.hpp
+++ DirectX 12 Rendering/Source/Core/DescriptorHeap.hpp
@@ -6,8 +6,20 @@
 
 struct Descriptor
 {
+private:
 	D3D12_CPU_DESCRIPTOR_HANDLE m_cpuHandle{};
 	D3D12_GPU_DESCRIPTOR_HANDLE m_gpuHandle{};
+
+public:
+	// Setter for CPU descriptor  handle
+	void SetCPU(D3D12_CPU_DESCRIPTOR_HANDLE Handle) { m_cpuHandle = Handle; }
+	// Setter for GPU descriptor  handle
+	void SetGPU(D3D12_GPU_DESCRIPTOR_HANDLE Handle) { m_gpuHandle = Handle; }
+
+	// Get CPU descriptor handle
+	[[nodiscard]] inline D3D12_CPU_DESCRIPTOR_HANDLE GetCPU() const { return m_cpuHandle; }
+	// Get GPU descriptor handle
+	[[nodiscard]] inline D3D12_GPU_DESCRIPTOR_HANDLE GetGPU() const { return m_gpuHandle; }
 
 	constexpr bool bIsValid() const { return m_cpuHandle.ptr != 0; }
 };
@@ -18,28 +30,43 @@ public:
 
 	void Allocate(Descriptor& targetDescriptor)
 	{
-		targetDescriptor.m_cpuHandle = GetCPU(m_Allocated);
-		targetDescriptor.m_gpuHandle = GetGPU(m_Allocated);
+		targetDescriptor.SetCPU(GetCPUptr(m_Allocated));
+		targetDescriptor.SetGPU(GetGPUptr(m_Allocated));
 		m_Allocated++;
 	}
 
-	D3D12_CPU_DESCRIPTOR_HANDLE GetCPU(uint32_t Index)
+	D3D12_CPU_DESCRIPTOR_HANDLE GetCPUptr(uint32_t Index)
 	{
 		return D3D12_CPU_DESCRIPTOR_HANDLE(m_Heap.Get()->GetCPUDescriptorHandleForHeapStart().ptr + Index * m_DescriptorSize);
 	}
 
-	D3D12_GPU_DESCRIPTOR_HANDLE GetGPU(uint32_t Index)
+	D3D12_GPU_DESCRIPTOR_HANDLE GetGPUptr(uint32_t Index)
 	{
 		return D3D12_GPU_DESCRIPTOR_HANDLE(m_Heap.Get()->GetGPUDescriptorHandleForHeapStart().ptr + Index * m_DescriptorSize);
 	}
 
+	inline ID3D12DescriptorHeap* GetHeap() const { return m_Heap.Get(); }
+	inline ID3D12DescriptorHeap** GetHeapAddessOf() { return m_Heap.GetAddressOf(); }
 
+	[[maybe_unused]] inline uint32_t GetDescriptorSize() const { return m_DescriptorSize; }
+	[[maybe_unused]] inline uint32_t GetDescriptorsCount() const { return m_NumDescriptors; }
+	[[maybe_unused]] inline uint32_t GetAllocatedCount() const { return m_Allocated; }
+
+	void SetDescriptorSize(const uint32_t NewSize)
+	{
+		m_DescriptorSize = NewSize;
+	}
+	void SetDescriptorsCount(const uint32_t Count)
+	{
+		m_NumDescriptors = Count;
+	}
+
+private:
 	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_Heap;
 	//D3D12_DESCRIPTOR_HEAP_TYPE m_Type;
 	uint32_t m_DescriptorSize{ 32 };
 	uint32_t m_NumDescriptors{ 0 };
 	uint32_t m_Allocated{ 0 };
-private:
 
 };
 
