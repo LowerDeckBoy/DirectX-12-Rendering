be5e46bbc7ca480c4622e9e7b6d4f0f317731341
LowerDeckBoy <pa.krawiec@onet.pl>
2023-04-10 19:14:45 +0200

    Update

--- DirectX 12 Rendering/Assets/Shaders/PS_GLTF.hlsl
+++ DirectX 12 Rendering/Assets/Shaders/PS_GLTF.hlsl
@@ -1,21 +1,28 @@
 
 struct PS_Input
 {
-    float4 Position     : SV_POSITION;
-    float4 WorldPosition : WORLD_POSITION;
-    float2 TexCoord     : TEXCOORD;
-    float3 Normal       : NORMAL;
-    float3 Tangent      : TANGENT;
-    float3 Bitangent    : BITANGENT;
+    float4 Position         : SV_POSITION;
+    float4 WorldPosition    : WORLD_POSITION;
+    float2 TexCoord         : TEXCOORD;
+    float3 Normal           : NORMAL;
+    float3 Tangent          : TANGENT;
+    float3 Bitangent        : BITANGENT;
 };
 
-Texture2D baseTexture : register(t0);
-Texture2D normalTexture : register(t1);
-SamplerState baseSampler : register(s0);
+Texture2D baseTexture           : register(t0);
+Texture2D normalTexture         : register(t1);
+Texture2D metallicTexture       : register(t2);
+Texture2D emissiveTexture       : register(t3);
+
+SamplerState baseSampler        : register(s0);
 
 float4 PS(PS_Input pin) : SV_TARGET
 {
+    return float4(0.5f, 0.5f, 0.5f, 1.0f);
     //return float4(1.0f, 1.0f, 1.0f, 1.0f);
-    //return float4(0.5f, 0.5f, 0.5f, 1.0f);
-	return baseTexture.Sample(baseSampler, pin.TexCoord);
+    
+    //if (baseTexture.Sample(baseSampler, pin.TexCoord).a < 0.1f)
+    //   discard;
+    
+    //return float4(baseTexture.Sample(baseSampler, pin.TexCoord));
 }
--- DirectX 12 Rendering/Assets/Shaders/VS_GLTF.hlsl
+++ DirectX 12 Rendering/Assets/Shaders/VS_GLTF.hlsl
@@ -1,16 +1,15 @@
 
-//struct cbPerObject
-//{
-//    row_major float4x4 WVP;
-//    row_major float4x4 World;
-//    float4 padding[8];
-//};
-
 cbuffer cbPerObject : register(b0)
 {
     row_major float4x4 WVP;
     row_major float4x4 World;
     float4 padding[8];
+}
+
+cbuffer cbCamera : register(b1)
+{
+    float3 CameraPosition;
+    float cameraPadding;
 }
 
 struct VS_Input
@@ -24,26 +23,27 @@ struct VS_Input
 
 struct VS_Output
 {
-    float4 Position     : SV_POSITION;
+    float4 Position      : SV_POSITION;
     float4 WorldPosition : WORLD_POSITION;
-    float2 TexCoord     : TEXCOORD;
-    float3 Normal       : NORMAL;
-    float3 Tangent      : TANGENT;
-    float3 Bitangent    : BITANGENT;
+    float3 ViewDirection : VIEW_DIRECTION;
+    float2 TexCoord      : TEXCOORD;
+    float3 Normal        : NORMAL;
+    float3 Tangent       : TANGENT;
+    float3 Bitangent     : BITANGENT;
 };
-
-//ConstantBuffer<cbPerObject> cbData : register(b0);
 
 VS_Output VS(VS_Input vin)
 {
     VS_Output output = (VS_Output) 0;
     
-    output.Position = mul(WVP, float4(vin.Position, 1.0f));
-    output.WorldPosition = mul(World, float4(vin.Position, 1.0f));
-    output.TexCoord  = vin.TexCoord; 
-    output.Normal    = normalize(mul((float3x3)World, vin.Normal));
-    output.Tangent   = normalize(mul((float3x3)World, vin.Tangent));
-    output.Bitangent = normalize(mul((float3x3)World, vin.Bitangent));
+    output.Position         = mul(WVP, float4(vin.Position, 1.0f));
+    output.WorldPosition    = mul(World, float4(vin.Position, 1.0f));
+    output.TexCoord         = vin.TexCoord; 
+    output.Normal           = normalize(mul((float3x3)World, vin.Normal));
+    output.Tangent          = normalize(mul((float3x3)World, vin.Tangent));
+    output.Bitangent        = normalize(mul((float3x3)World, vin.Bitangent));
+    
+    output.ViewDirection = normalize(CameraPosition - output.WorldPosition.xyz);
     
 	return output;
 }
