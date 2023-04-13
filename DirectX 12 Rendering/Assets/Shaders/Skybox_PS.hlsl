087d7c964921e03c88137a4f837fbe53ebb81f0f
LowerDeckBoy <pa.krawiec@onet.pl>
2023-04-13 17:23:37 +0200

    Updates

--- DirectX 12 Rendering/Assets/Shaders/Skybox_PS.hlsl
+++ DirectX 12 Rendering/Assets/Shaders/Skybox_PS.hlsl
@@ -10,10 +10,25 @@ struct PS_INPUT
     float3 TexCoord : TEXCOORD;
 };
 
-TextureCube skyboxTexture : register(t0);
+static const float2 invAtan = float2(0.1591f, 0.3183f);
+// Converting equirectangular map (spherical) into cube format
+// required if skybox is of cube shape instead of sphere
+float2 EquirectangularToCube(float3 v)
+{
+    float2 uv = float2(atan2(v.z, v.x), -asin(v.y));
+    uv *= invAtan;
+    uv += 0.5f;
+    return uv;
+}
+
+//TextureCube skyboxTexture : register(t0);
+Texture2D skyboxTexture : register(t0);
 SamplerState texSampler : register(s0);
 
 float4 PS(PS_INPUT pin) : SV_TARGET
 {
-    return skyboxTexture.Sample(texSampler, pin.TexCoord);
+    float2 uv = EquirectangularToCube(normalize(pin.TexCoord));
+    float3 skyTexture = skyboxTexture.Sample(texSampler, uv).rgb;
+    return float4(skyTexture, 1.0f);
+    //return float4(skyboxTexture.Sample(texSampler, pin.TexCoord.xy).rgb, 1.0f);
 }
