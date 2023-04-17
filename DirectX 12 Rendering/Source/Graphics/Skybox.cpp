78eabd8e20d84ec24b222dfc1c98de596913e6ae
LowerDeckBoy <pa.krawiec@onet.pl>
2023-04-17 12:58:32 +0200

    New textures for testing

--- DirectX 12 Rendering/Source/Graphics/Skybox.cpp
+++ DirectX 12 Rendering/Source/Graphics/Skybox.cpp
@@ -13,7 +13,12 @@ void Skybox::Create(Device* pDevice)
 	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/earth-cubemap.dds");
 	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/SunSubMixer_specularIBL.dds");
 	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/newport_loft.hdr");
-	m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/anime_art_style_magic_forest_with_intense_blue_lig.hdr");
+	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/anime_art_style_magic_forest_with_intense_blue_lig.hdr");
+	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/HDR/anime_art_style_sunset_by_the_lake.hdr");
+	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/HDR/fantasy_lands_bright_nightsky_by_the_lake.hdr");
+	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/HDR/fantasy_lands_sunny_day_skymap_by_the_purple_star_.hdr");
+	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/HDR/fantasy_lands_sunset_by_the_lake.hdr");
+	m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/HDR/fantasy_landscape_nightsky.hdr");
 	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/PaperMill_E_Env.hdr");
 	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/anime_art_style_sunny_day_sky.jpg");
 	//m_Texture = std::make_unique<Texture>(pDevice, "Assets/Textures/fantasy_landscape_nightsky.jpeg");
