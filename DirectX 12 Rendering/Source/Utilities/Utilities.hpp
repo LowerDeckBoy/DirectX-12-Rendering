acaf9794da1d53ee669c2da0003cfa94215eab43
LowerDeckBoy <pa.krawiec@onet.pl>
2023-09-12 16:35:29 +0200

    Changed and improved debug helper functions into macros

--- DirectX 12 Rendering/Source/Utilities/Utilities.hpp
+++ DirectX 12 Rendering/Source/Utilities/Utilities.hpp
@@ -2,42 +2,39 @@
 #include <string>
 #include <stdexcept>
 #include <cassert>
+
+#if defined (_DEBUG) || (DEBUG)
 #include <debugapi.h>
+#endif
 
 // For ComPtr
+#ifndef SAFE_RELEASE
 #define SAFE_RELEASE(x) { if (x) { x.Reset(); x = nullptr; } }
+#endif
 // For non-ComPtr
+#ifndef SAFE_DELETE
 #define SAFE_DELETE(x) { if (x) { x->Release(); x = nullptr; } }
+#endif
 
-#define TraceError(x)													\
-	if (FAILED(x)) {													\
-		std::string message{ "HRESULT failed\nFile: " + std::string(__FILE__) + "\nLine: " + std::to_string(__LINE__) + '\n' };	\
-		::OutputDebugStringA(message.c_str());							\
-		::MessageBoxA(nullptr, message.data(), "Error", MB_OK);			\
-		throw std::runtime_error("Exception thrown");					\
-	}	
+// Helper macro to determinate where exactly an HRESULT error occurs
+#ifndef TraceError
+#define TraceError(DebugMessage)											\
+	{																		\
+		std::string file{ "File: " + std::string(__FILE__) };				\
+		std::string func{ "\nFunction: " + std::string(__func__) };			\
+		std::string line{ "\nLine: " + std::to_string(__LINE__) + '\n' };	\
+		std::string message{ file + func + line + DebugMessage };			\
+		::OutputDebugStringA(message.c_str());								\
+		::MessageBoxA(nullptr, message.data(), "Error", MB_OK);				\
+	}																	
+#endif
 
-inline void ThrowIfFailed(HRESULT hResult)
-{
-	if (FAILED(hResult))
-		throw std::runtime_error("Exception thrown");
-}
-
-inline void ThrowIfFailed(HRESULT hResult, const std::string_view& Msg)
-{
-	if (FAILED(hResult))
-	{
-		const auto msg{ "Error in: " + std::string(__FILE__) + ", at line: " + std::to_string(__LINE__) };
-		::MessageBoxA(nullptr, msg.data(), "Error", MB_OK);
-		//::MessageBoxA(nullptr, Msg.data(), "Error", MB_OK);
-		throw std::runtime_error(Msg.data());
-	}
-}
-
-[[maybe_unused]]
-inline const wchar_t* ToWchar(const std::string& String)
-{
-	std::wstring wpath = std::wstring(String.begin(), String.end());
-	auto output{ wpath.c_str() };
-	return output;
-}
+#ifndef ThrowIfFailed
+#define ThrowIfFailed(hResult, ...)									\
+		if (FAILED(hResult))										\
+		{															\
+			std::string msg{ std::string(__VA_ARGS__) };			\
+			TraceError(msg);										\
+			throw std::runtime_error("HRESULT exception thrown.");	\
+		}
+#endif
